a440ce20b32218d243a0a198868148ca0afbead3
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-11-24 11:31:05 +0300

    add new directory

--- app/view.py
+++ app/view.py
@@ -7,8 +7,8 @@ def index():
     '''
     view root funtion that returns the index page and its data
     '''
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    title = 'Home - welcome to the best movie review website Online '
+    return render_template('index.html',title = title)
 
 @app.route('/movie/<int:moive_id>')
 def movie(movie_id):
@@ -16,5 +16,5 @@ def movie(movie_id):
     view movie page function that returns the movie details page and its data 
     '''
 
-    
+
     return render_template('movie.html',id = movie_id)
